754938bce0fca1a391c52e05af0b69c4ca6e2000
GERMANISAAC1 <ISAACLUNA752@GMAIL.COM>
2026-05-10 06:16:08 -0500

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,22 +1,21 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
 android {
-<<<<<<< HEAD
-<<<<<<< HEAD
     namespace = "com.example.barberpro"
-=======
-    namespace = "com.example.peluqueria_kety"
->>>>>>> 4059aac1e3a0f823b063f7bb6b173b93875a43c2
-=======
-    namespace = "com.example.barberpro_unicorn"
->>>>>>> Initial commit
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
+
+    defaultConfig {
+        applicationId = "com.example.barberpro"
+        minSdk = 21
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+    }
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
@@ -24,32 +23,11 @@ android {
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
-    }
-
-    defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-<<<<<<< HEAD
-<<<<<<< HEAD
-        applicationId = "com.example.barberpro"
-=======
-        applicationId = "com.example.peluqueria_kety"
->>>>>>> 4059aac1e3a0f823b063f7bb6b173b93875a43c2
-=======
-        applicationId = "com.example.barberpro_unicorn"
->>>>>>> Initial commit
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
+        jvmTarget = "17"
     }
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }

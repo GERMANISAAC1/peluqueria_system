81195b19b873c66cf5e7b203a2ea5563d24b1163
GERMANISAAC1 <ISAACLUNA752@GMAIL.COM>
2026-08-29 07:25:02 -0500

    Refactor namespace assignment for Android library plugins

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -21,28 +21,25 @@ subprojects {
 
 // -----------------------------------------------------------------------
 // FIX: algunos plugins antiguos (como device_apps) no declaran "namespace"
-// en su build.gradle, algo que AGP 8+ exige obligatoriamente. Este bloque
-// lo detecta leyendo el atributo "package" del AndroidManifest.xml de
-// cada subproyecto de tipo librería y se lo asigna automáticamente si
-// falta, sin tocar el código del plugin en sí.
+// en su build.gradle, algo que AGP 8+ exige. Usamos plugins.withId en vez
+// de afterEvaluate para evitar conflicto con evaluationDependsOn de arriba
+// (afterEvaluate revienta con "project already evaluated" en ese escenario).
 // -----------------------------------------------------------------------
 subprojects {
-    afterEvaluate {
-        if (project.plugins.hasPlugin("com.android.library")) {
-            val androidExt = project.extensions.findByType(
-                com.android.build.gradle.LibraryExtension::class.java
-            )
-            if (androidExt != null && androidExt.namespace == null) {
-                val manifestFile = project.file("src/main/AndroidManifest.xml")
-                if (manifestFile.exists()) {
-                    val doc = javax.xml.parsers.DocumentBuilderFactory
-                        .newInstance()
-                        .newDocumentBuilder()
-                        .parse(manifestFile)
-                    val packageName = doc.documentElement.getAttribute("package")
-                    if (packageName.isNotEmpty()) {
-                        androidExt.namespace = packageName
-                    }
+    plugins.withId("com.android.library") {
+        val androidExt = extensions.getByType(
+            com.android.build.gradle.LibraryExtension::class.java
+        )
+        if (androidExt.namespace == null) {
+            val manifestFile = file("src/main/AndroidManifest.xml")
+            if (manifestFile.exists()) {
+                val doc = javax.xml.parsers.DocumentBuilderFactory
+                    .newInstance()
+                    .newDocumentBuilder()
+                    .parse(manifestFile)
+                val packageName = doc.documentElement.getAttribute("package")
+                if (packageName.isNotEmpty()) {
+                    androidExt.namespace = packageName
                 }
             }
         }
